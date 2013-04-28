6ae044d68ac64ad49244c3f0f2d9632cd7029b3d
t.hebert73 <t.hebert73@gmail.com>
2013-04-28 12:56:54 -0400

    first commit
    This basically shows a game menu screen. The game loop is created. The
    user can't do anything besides select 'Quit'.

---
+++ src/main/Game.java
@@ -0,0 +1,16 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Game {
+	
+	public static void main(String[] args) {
+		
+		javax.swing.JFrame window = new JFrame("Zombie Killer");
+		window.setContentPane(new GamePanel());
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.pack();
+		window.setVisible(true);
+	}
+}
